76bb4a3181c248e55968f5f284428cd2a11ba485
Sunil <sunil9766@gmail.com>
2018-06-03 15:53:32 +0530

    moved minor urls changes

---
+++ server/menus/api/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+from django.urls import path, re_path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_jwt.views import obtain_jwt_token
+
+from menus.api import views as menus_views
+
+router = DefaultRouter()
+router.register(r'category', menus_views.MenuCategoryViewSet)
+router.register(r'item', menus_views.MenuItemViewSet)
+
+app_name = 'menus'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
